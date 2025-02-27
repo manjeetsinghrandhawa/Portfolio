d2a980240434a248b5233b36099c37a569e1f50a
manjeetsinghh05 <138310696+manjeetsinghh05@users.noreply.github.com>
2025-02-27 20:28:44 +0530

    mail correction done

--- components/sections/ContactSection.tsx
+++ components/sections/ContactSection.tsx
@@ -107,7 +107,7 @@ const ContactForm = () => {
       <div className="flex flex-col items-center justify-center p-6 ">
         <p className="text-blue-400 items-center ">or</p>
         <a
-          href="mailto:your-email@example.com?subject=Contact%20Query"
+          href="mailto:manjeetsinghh052003@gmail.com?subject=Contact%20Query"
           className="flex items-center justify-center mt-5 w-[95%] gap-2 px-4 py-2 bg-gray-800 hover:bg-gray-700 text-white rounded-lg shadow-md transition duration-300"
         >
           <FaEnvelope className="text-blue-400 " />
