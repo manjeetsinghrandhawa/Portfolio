abf42fcb60db0d2e68e6fda2ea1da7ba0d64ab92
manjeetsinghh05 <138310696+manjeetsinghh05@users.noreply.github.com>
2025-02-27 16:19:27 +0530

    metadata_done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,14 @@
 import Navbar from "@/components/Navbar";
 import "./globals.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Manjeet's Portfolio",
+  description: "This is Manjeet's Portfolio.",
+  icons: {
+    icon: "/assets/manjeetphotoedited.jpg",
+  },
+};
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
